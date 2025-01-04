1aac23df9c8b685883a37c0bfcc7569e5dcacb57
Daxpithiya <86251109+Daxpithiya@users.noreply.github.com>
2025-01-04 23:59:44 +0530

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-// Hardcoded credentials (use a secure backend in real applications)
-const validUsername = "test";
-const validPassword = "test";
-
-function login() {
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
-  const errorMsg = document.getElementById("error-msg");
-
-  if (username === validUsername && password === validPassword) {
-    errorMsg.textContent = "";
-    // Redirect to the second page
-    window.location.href = "https://www.google.co.in";
-  } else {
-    errorMsg.textContent = "Invalid username or password.";
-  }
-}
+echo "577";
